eff563357f9bcc1b0a186d07066ae3fe662f2b0e
NikitaKST <8172535@google.com>
2024-03-02 18:05:42 +0500

    Task complete

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { ShopItemFunc } from './components/ShopItemFunc';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='container'>
+      <div className='background-element'></div>
+      <div className='highlight-window'>
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className='window'>
+        <ShopItemFunc />
+      </div>
+    </div>
+  );
+}
+
+export default App;
